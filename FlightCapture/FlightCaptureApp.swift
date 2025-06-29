387ef19a77ff8ce75786c50b31598ca7a2ddb42b
Kevin Smith <kevzaddress@gmail.com>
2025-06-29 20:14:46 +0300

    Initial Commit

---
+++ FlightCapture/FlightCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightCaptureApp.swift
+//  FlightCapture
+//
+//  Created by Kevin Smith on 29/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct FlightCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
